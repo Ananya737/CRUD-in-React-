c4888f82ec392b4a60e2dc1d454e171626dc37bc
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-23 00:48:13 +0530

    Display done

--- MyApp/src/Pages/Display.jsx
+++ MyApp/src/Pages/Display.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
+
 
 
 
@@ -10,16 +12,50 @@ const Display=()=>{
         let api='http://localhost:3000/Students';
         let res=await axios.get(api);
         console.log(res.data);
-
-
+        setMyData(res.data);
     }
     useEffect(()=>{
         display();
     },[])
 
+    let ans=myData.map((key)=>{
+        let srno=0;
+        return(
+            <>
+            <tr>
+                <td>{srno++}</td>
+                <td>{key.id}</td>
+                <td>{key.empno}</td>
+                <td>{key.name}</td>
+                <td>{key.city}</td>
+                <td>{key.salary}</td>
+            </tr>
+            </>
+        )
+    })
+
     return(
         <>
         <h1 style={{display:"flex",justifyContent:"center",backgroundColor:"bisque",padding:"20px"}}>Display Page!</h1>
+
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Sr. No.</th>
+          <th>ID</th>
+          <th>Employee no.</th>
+          <th>Name</th>
+          <th>City</th>
+          <th>Salary</th>
+        </tr>
+        
+      </thead>
+      <tbody>
+      {ans}
+      </tbody>
+      
+    </Table>
+
 
 
 
