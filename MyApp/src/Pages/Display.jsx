0621bff78edeebb1ebe48fb5239c61167a05fee7
Ananya Sahu <ananyasahu59513@gmail.com>
2025-04-23 00:37:41 +0530

    Display in process

--- MyApp/src/Pages/Display.jsx
+++ MyApp/src/Pages/Display.jsx
@@ -1,8 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
 
 const Display=()=>{
+    const [myData,setMyData]=useState([]);
+
+    let display=async()=>{
+        let api='http://localhost:3000/Students';
+        let res=await axios.get(api);
+        console.log(res.data);
+
+
+    }
+    useEffect(()=>{
+        display();
+    },[])
+
     return(
         <>
         <h1 style={{display:"flex",justifyContent:"center",backgroundColor:"bisque",padding:"20px"}}>Display Page!</h1>
+
+
+
+
+
         
         </>
     )
